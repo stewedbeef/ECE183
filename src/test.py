c9ff5e41a0f318c5f26dfb9d1ba447ac44639f22
stewedbeef <114018527+stewedbeef@users.noreply.github.com>
2024-06-01 16:23:29 -0700

    fix arcane error and write proper python test file

--- src/test.py
+++ src/test.py
@@ -2,47 +2,27 @@ import cocotb
 from cocotb.clock import Clock
 from cocotb.triggers import RisingEdge, FallingEdge, Timer, ClockCycles
 
-
-segments = [ 63, 6, 91, 79, 102, 109, 125, 7, 127, 111 ]
-
 @cocotb.test()
-async def test_7seg(dut):
+async def test_my_design(dut):
+
+    CONSTANT_CURRENT = 40
     dut._log.info("start")
-    clock = Clock(dut.clk, 10, units="us")
-    cocotb.start_soon(clock.start())
 
-    # reset
-    dut._log.info("reset")
-    dut.rst_n.value = 0
-    # set the compare value
-    dut.ui_in.value = 1
+    clock = Clock(dut.clk, 1, units="ns") # Create a 1ns period clock on port clk
+    cocotb.start_soon(clock.start()) # Start the clock
+
+    dut.rst_n.value = 0 # low to reset
     await ClockCycles(dut.clk, 10)
-    dut.rst_n.value = 1
+    dut.rst_n.value = 1 # assert reset
 
-    # the compare value is shifted 10 bits inside the design to allow slower counting
-    max_count = dut.ui_in.value << 10
-    dut._log.info(f"check all segments with MAX_COUNT set to {max_count}")
-    # check all segments and roll over
-    for i in range(15):
-        dut._log.info("check segment {}".format(i))
-        await ClockCycles(dut.clk, max_count)
-        assert int(dut.segments.value) == segments[i % 10]
+    dut.ui_in.value = CONSTANT_CURRENT
+    dut.ena.value = 1 # enable design
 
-        # all bidirectionals are set to output
-        assert dut.uio_oe == 0xFF
-
-    # reset
-    dut.rst_n.value = 0
-    # set a different compare value
-    dut.ui_in.value = 3
-    await ClockCycles(dut.clk, 10)
-    dut.rst_n.value = 1
-
-    max_count = dut.ui_in.value << 10
-    dut._log.info(f"check all segments with MAX_COUNT set to {max_count}")
-    # check all segments and roll over
-    for i in range(15):
-        dut._log.info("check segment {}".format(i))
-        await ClockCycles(dut.clk, max_count)
-        assert int(dut.segments.value) == segments[i % 10]
-
+    # await ClockCycles(dut.clk, 100)
+    for _ in range(100):
+        await RisingEdge(dut.clk)
+        # dut._log.info(f"ui_in: {dut.ui_in.value}, ui_out: {dut.ui_out.value}")
+                      
+    dut._log.info("Finished Test")
+    
+    dut.ena.value = 0 # disable design
